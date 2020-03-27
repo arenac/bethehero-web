3064647674a77aa68c3aff0e0da117c680a8f315
Nilo Neregato <neregato.nilo@gmail.com>
2020-03-27 22:35:43 +0100

    register page

---
+++ src/pages/Register/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import {
+  Container,
+  Content,
+  Section,
+  Logo,
+  Form,
+  InputContainer,
+} from './styles';
+
+import Button from '../../common/Button';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Register() {
+  return (
+    <Container>
+      <Content>
+        <Section>
+          <Logo src={logoImg} alt="Be The Hero" />
+
+          <h1>Register</h1>
+          <p>Create your account and help people find your ONG.</p>
+          <Link to="/">
+            <FiArrowLeft size={16} color="#e02041" />
+            Login
+          </Link>
+        </Section>
+        <Form>
+          <input placeholder="ONG name" />
+          <input tipe="email" placeholder="E-mail" />
+          <input placeholder="WhatsApp" />
+          <InputContainer>
+            <input placeholder="City" />
+            <input placeholder="CO" style={{ width: 80 }} />
+          </InputContainer>
+          <Button
+            type="submit"
+            text="Register"
+            handleClick={() => {
+              alert('register');
+            }}
+          />
+        </Form>
+      </Content>
+    </Container>
+  );
+}
