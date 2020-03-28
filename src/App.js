97858160e951a244c7b1f9a56ea8193040ea0786
Nilo Neregato <neregato.nilo@gmail.com>
2020-03-29 00:07:42 +0100

    create history js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
+import history from './services/history';
 import { store, persistor } from './store';
 
 import GlobalStyle from './css/global';
@@ -13,8 +15,10 @@ function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <GlobalStyle />
-        <Routes />
+        <Router history={history}>
+          <GlobalStyle />
+          <Routes />
+        </Router>
       </PersistGate>
     </Provider>
   );
