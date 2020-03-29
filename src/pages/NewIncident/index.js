bbd2db1a7cc9a819b1c66f10ccdfda15ea97f524
Nilo Neregato <neregato.nilo@gmail.com>
2020-03-29 01:18:45 +0100

    new incident

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import { Container, Content, Section, Logo, Form } from './styles';
@@ -8,7 +8,37 @@ import Button from '../../common/Button';
 
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+      history.push('/profile');
+    } catch (error) {
+      alert('Error when adding new incident');
+    }
+  }
+
   return (
     <Container>
       <Content>
@@ -22,18 +52,24 @@ export default function NewIncident() {
             Home
           </Link>
         </Section>
-        <Form>
-          <input placeholder="Title" />
-          <textarea placeholder="Description" />
-          <input placeholder="Value in $" />
-
-          <Button
-            type="submit"
-            text="Register"
-            handleClick={() => {
-              alert('register');
-            }}
+        <Form onSubmit={handleNewIncident} action="">
+          <input
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
+          <textarea
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Value in $"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+
+          <Button type="submit" text="Register" />
         </Form>
       </Content>
     </Container>
